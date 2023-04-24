5febed2339e93e4cf65e45b5f6481ebab3333068
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-04-24 11:20:20 +0200

    feature: added getstaticparams

--- app/(default_layout)/blog/[slug]/page.tsx
+++ app/(default_layout)/blog/[slug]/page.tsx
@@ -6,6 +6,9 @@ type Props = {
     slug: string;
   };
 };
+
+const WP_REST_BASE = process.env.WP_REST_BASE
+
 export default async function BlogPostPage({ params: { slug } }: Props) {
   const response = await fetch(
     `https://react.webworker.berlin/wp-json/wp/v2/posts?slug=${slug}`
@@ -66,3 +69,21 @@ async function getImageData(imageId: number) {
 
   return imageData;
 }
+
+
+/* Mit dieser Funktion können alle Werte von slug, die aktuell bekannt sind,
+schon vorab an Next mitgeteilt werden, so dass die Seiten für diese Slugs
+schon beim build erzeugt werden können, und nicht dynamisch beim ersten Aufrufen
+des Slugs (mit Wartezeit) erzeugt werden müssen.
+https://beta.nextjs.org/docs/api-reference/generate-static-params
+*/
+export async function generateStaticParams() {
+  const response = await fetch(`${WP_REST_BASE}/posts`);
+
+  const posts = (await response.json()) as BlogPostRest[];
+
+  return posts.map((post) => ({
+	slug: post.slug,
+  }));
+}
+
